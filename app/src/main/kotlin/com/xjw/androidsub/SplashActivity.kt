65aa388a81df043a345cc40d51deba4dbd3f1d77
xjw <tony_stonner@163.com>
2020-08-09 17:49:56 +0800

    init.

---
+++ app/src/main/kotlin/com/xjw/androidsub/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.xjw.androidsub
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 2020/8/9 17:41
+ *
+ * @author <a href="1521975316@qq.com">xjw</a>
+ * @Record:
+ */
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
